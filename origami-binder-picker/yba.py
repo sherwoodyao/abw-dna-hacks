aa9512a57c5483b6f0a49be79597990ffcae3b49
pythonmonke <chessbro812@gmail.com>
2021-06-24 17:45:15 -0400

    6/24 5:45 PM update
    graphing changes more functions etc

--- origami-binder-picker/yba.py
+++ origami-binder-picker/yba.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
-# from colorama import Fore, Style
 import csv
 import pandas
 
 input_ex = [[1, 1, 2, "A"], [2, 2, 3, "B"], [3, 3, 4, "C"]]
-input_ex_v2 = [[1, 1, 2, "A", "I", "3'"], [2, 2, 3, "B", "O", "3'"], [3, 3, 4, "C", "I", "5'"]]
+input_ex_v2 = [[1, 3, 95, "A", "I", "3'"], [2, 1, 2, "B", "O", "3'"], [3, 3, 4, "C", "I", "5'"]]
 oligo_dict_ex = {
     "A": "AAAAAAAAAAAAAAA",
     "B": "GGGGGGGGGGGGGGG",
@@ -14,7 +13,12 @@ oligo_dict_ex = {
 }
 
 
-def checklist(input_list, user_y=None, user_z=None, user_oligo=None, user_direc=None, user_prime=None):
+def sort(sub_li, num):
+    sub_li.sort(key=lambda x: x[num])
+    return sub_li
+
+
+def checklist(input_list, user_y='', user_z='', user_oligo='', user_direc='', user_prime=''):
     t1 = False
     t2 = False
     t3 = False
@@ -25,27 +29,43 @@ def checklist(input_list, user_y=None, user_z=None, user_oligo=None, user_direc=
     multiple_cut = user_oligo.split(",")
     user_oligo_list = [x.replace(" ", "") for x in multiple_cut]
 
-    if user_y or user_z or user_oligo or user_direc or user_prime == '':
-        pass
+    if user_y == '':
+        t1 = True
     else:
         for x in range(int(range_y[0]), int(range_y[1]) + 1):
             if input_list[1] == x:
                 t1 = True
                 continue
 
+    if user_z == '':
+        t2 = True
+    else:
         for x in range(int(range_z[0]), int(range_z[1]) + 1):
             if input_list[2] == x:
                 t2 = True
                 continue
 
+    if user_oligo == '':
+        t3 = True
+    else:
         for x in user_oligo_list:
             if input_list[3] == x:
                 t3 = True
                 continue
 
-        if input_list[4] == user_direc:
+    if user_direc == '':
+        t4 = True
+    else:
+        if user_direc == "U" or user_direc == "u":
+            user_direc = "I"
+        elif user_direc == "D" or user_direc == "d":
+            user_direc = "O"
+        if user_direc == input_list[4]:
             t4 = True
 
+    if user_prime == '':
+        t5 = True
+    else:
         if input_list[5] == user_prime:
             t5 = True
 
@@ -59,78 +79,123 @@ def checklist(input_list, user_y=None, user_z=None, user_oligo=None, user_direc=
 
 def dict_graph_creator(input_list):
     input_dict = {}
-    user_range_y = ''
-    user_range_z = ''
-    user_oligo_id = ''
-    user_direction = ''
-    user_prime = ''
+    range_ys = ['']
+    range_zs = ['']
+    oligo_ids = ['']
+    directions = ['']
+    primes = ['']
     graph_runner = True
 
     # Reformats the list into a dictionary
-    for z in range(1, len(input_list)+1):
-        input_dict[str(z)] = input_list[z-1]
+    for z in range(1, len(input_list) + 1):
+        input_dict[str(z)] = input_list[z - 1]
 
     while graph_runner:
-        # Hello
-        main_prompt = input("Restrictions? [Y] or [N]: ")
+        # User selects/inputs restrictions
+        main_prompt = input("[A]dd restrictions, [R]emove restrictions or [G]raph: ")
 
-        if main_prompt == "Y" or main_prompt == "y":
-            menu_select = input("Select filter: Range_[Y], Range_[Z], [O]ligo ID, [U]p/down, or [P]rime: ")
+        if main_prompt == "A" or main_prompt == "a":
+            menu_select = input("Select filter: Range_[Y], Range_[Z], [O]ligo ID, [D]irection, or 3/5 [P]rime: ")
 
             if menu_select == "Y" or menu_select == "y":
-                user_range_y = input("Range of Y values? (Ex: 1-3): ")
+                sort(input_list, 1)
+                user_range_y = input(f"Range of Y values? ({input_list[0][1]}-{input_list[-1][1]}nm): ")
+                range_y = user_range_y.split("-")
+
+                if len(range_y) == 1:
+                    print("Command only accepts range.")
+                    continue
+                else:
+                    range_ys.append(user_range_y)
+
             elif menu_select == "Z" or menu_select == "z":
-                user_range_z = input("Range of Z values?: ")
+                sort(input_list, 2)
+                user_range_z = input(f"Range of Z values? ({input_list[0][2]}-{input_list[-1][2]}nm): ")
+                range_z = user_range_z.split("-")
+
+                if len(range_z) == 1:
+                    print("Command only accepts range.")
+                    continue
+                else:
+                    range_zs.append(user_range_z)
+
             elif menu_select == "O" or menu_select == "o":
                 user_oligo_id = input("Oligo IDs (Ex: A, B, C): ")
-            elif menu_select == "U" or menu_select == "u":
-                user_direction = input("Up or down? [I]/[O]: ")
+                oligo_ids.append(user_oligo_id)
+
+            elif menu_select == "D" or menu_select == "d":
+                user_direction = input("[U]p or [D]own?: ")
+                directions.append(user_direction)
+
             elif menu_select == "P" or menu_select == "p":
                 user_prime = input("[5'] or [3']?: ")
+                primes.append(user_prime)
+            else:
+                print("Invalid input.")
 
-        # Base construction of graph
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        # plt.ion()
-        for x in input_list:
-            color = ""
-            marker = ""
+        elif main_prompt == "R" or main_prompt == "r":
+            range_ys.clear()
+            range_zs.clear()
+            oligo_ids.clear()
+            directions.clear()
+            primes.clear()
+            range_ys = ['']
+            range_zs = ['']
+            oligo_ids = ['']
+            directions = ['']
+            primes = ['']
 
-            if x[4] == "I":
-                color = "blue"
-            elif x[4] == "O":
-                color = "red"
+        elif main_prompt == "G" or main_prompt == "g":
+            # Base construction of graph
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            # plt.ion()
 
-            if x[5] == "5'":
-                marker = 'o'
-            elif x[5] == "3'":
-                marker = ','
+            for x in input_list:
+                color = ""
+                marker = ""
 
-            # Set up legend
-            colors = ["red", "blue"]
-            circles = [Line2D([0], [0], color=x, marker="o", linewidth=0) for x in colors]
-            squares = [Line2D([0], [0], color=x, marker="s", linewidth=0) for x in colors]
-            shapes = circles + squares
+                if x[4] == "I":
+                    color = "blue"
+                elif x[4] == "O":
+                    color = "red"
 
-            # Plot/create legend if meets filters or if no filters
-            if checklist(user_range_y, user_range_z, user_oligo_id, user_direction, user_prime, x)\
-                    or main_prompt == "N" or main_prompt == "n":
+                if x[5] == "5'":
+                    marker = 'o'
+                elif x[5] == "3'":
+                    marker = ','
+
+                # Set up legend
+                colors = ["red", "blue"]
+                circles = [Line2D([0], [0], color=x, marker="o", linewidth=0) for x in colors]
+                squares = [Line2D([0], [0], color=x, marker="s", linewidth=0) for x in colors]
+                shapes = circles + squares
                 # plt.draw()
                 # plt.pause(0.001)
-                ax.scatter([x[1]], [x[2]], color=color, marker=marker)
-                ax.legend(shapes, ["5' down", "5' up", "3' down", "3' up"], bbox_to_anchor=(1.1, 1.05))
 
-                # Marks the points
-                for a, b in input_dict.items():
-                    if x[1] in b and x[2] in b:
-                        ax.annotate(a, (x[1]+0.06, x[2]+0.02))
+                # sort1 = sort(input_list, 1)
+                # sort2 = sort(input_list, 2)
+                # ax.set_xlim([sort1[0][1]-2, sort1[-1][1]+2])
+                # ax.set_ylim([sort2[0][2]-2, sort2[-1][2]+2])
 
-        # print(input_dict)
-        plt.show()
+                # Plot/create legend if meets filters or if no filters
+                if checklist(x, range_ys[-1], range_zs[-1], oligo_ids[-1], directions[-1], primes[-1]):
+                    ax.scatter([x[1]], [x[2]], color=color, marker=marker)
+                    ax.legend(shapes, ["5' down", "5' up", "3' down", "3' up"], bbox_to_anchor=(1.1, 1.05))
+
+                    # Marks the points
+                    for a, b in input_dict.items():
+                        if x[1] == b[1] and x[2] == b[2]:
+                            ax.annotate(a, (x[1] + 0.0079*x_len, x[2] + 0.0079*y_len))
+
+            # print(input_dict)
+            plt.show()
     return input_dict
 
 
 conversion = dict_graph_creator(input_ex_v2)
+
+
 # Function that sets up the modified oligonucleotide dictionary
 # Uses the dictionary produced from dict_graph_creator and an oligonucleotide dict
 
@@ -178,6 +243,7 @@ def modify_input(input_dict, oligo_dict):
             elif edit_input == "V" or edit_input == "v":
                 if len(mod_dict) == 0:
                     print("There are no modifications currently loaded.")
+
                 else:
                     first = next(iter(mod_dict))
                     for xy, zy in mod_dict.items():
@@ -191,17 +257,21 @@ def modify_input(input_dict, oligo_dict):
                 print("Modifications:")
                 print(*key_display, sep=", ")
                 delete_prompt = input(f"Delete [A]ll or [S]elect?: ")
+
                 if delete_prompt == "A" or delete_prompt == "a":
                     mod_dict.clear()
                     key_display.clear()
+
                 elif delete_prompt == "S" or delete_prompt == "s":
                     select_delete = input("Which would you like to delete?: ")
                     delete_split = select_delete.split(",")
                     delete_split_fix = [x.replace(" ", "") for x in delete_split]
+
                     try:
                         for b in delete_split_fix:
                             del mod_dict[b]
                             key_display.remove(b)
+
                     except KeyError:
                         print("Selection does not exist.")
                 else:
@@ -210,15 +280,18 @@ def modify_input(input_dict, oligo_dict):
             # Save modifications dict to csv file
             elif edit_input == "S" or edit_input == "s":
                 save_input = input("What file would you like to save to? (Ex: modifications): ")
+
                 with open(f"{save_input}.csv", "w") as file:
                     writer = csv.writer(file)
                     for key, value in mod_dict.items():
                         writer.writerow([key, value])
+
                 print("Successfully saved.")
 
             # Load modifications data from csv file
             elif edit_input == "L" or edit_input == "l":
                 load_input = input("What file would you like to load? (Ex: modifications): ")
+
                 try:
                     mod_data = pandas.read_csv(f"{load_input}.csv").to_dict()
                     mod_df = pandas.DataFrame(mod_data)
@@ -227,10 +300,13 @@ def modify_input(input_dict, oligo_dict):
                     for modifier, strand in mod_dict.items():
                         if modifier != "modifier" and modifier not in key_display:
                             key_display.append(modifier)
+
                 except FileNotFoundError:
                     print("No records of previously saved files.")
+
                 except AttributeError:
                     print("File corrupted, select another.")
+
                 else:
                     print("Successfully loaded.")
 
@@ -271,6 +347,7 @@ def modify_input(input_dict, oligo_dict):
                                 if prime_select == "3'":
                                     modified_oligo_dict[input_dict[x][3]] = \
                                         oligo_dict[input_dict[x][3]] + mod_dict[mod_select]
+
                                 elif prime_select == "5'":
                                     modified_oligo_dict[input_dict[x][3]] = \
                                         mod_dict[mod_select] + oligo_dict[input_dict[x][3]]
@@ -301,6 +378,7 @@ def modify_input(input_dict, oligo_dict):
                                 if prime_select == "3'":
                                     modified_oligo_dict[input_dict[zb][3]] = \
                                         oligo_dict[input_dict[zb][3]] + mod_dict[mod_select]
+
                                 elif prime_select == "5'":
                                     modified_oligo_dict[input_dict[zb][3]] = \
                                         mod_dict[mod_select] + oligo_dict[input_dict[zb][3]]
@@ -346,6 +424,5 @@ def modify_input(input_dict, oligo_dict):
             else:
                 print("Successfully loaded.")
     return modified_oligo_dict
-
 
 # modify_input(conversion, oligo_dict_ex)
